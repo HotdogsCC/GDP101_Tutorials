b2f7d1cedde7a2264482f3fe1005816b4691dbb5
Charlie Campbell <chacha9669@hotmail.com>
2024-09-07 19:12:01 +1000

    Finished Week 6

--- Week6/Week6.cpp
+++ Week6/Week6.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
+
+void Task2()
+{
+	string inputStr;
+	cout << "Enter a string which at least 20 characters: " << "\n";
+	getline(cin, inputStr);
+	if (size(inputStr) < 20)
+	{
+		cout << "Come on man, you had one job. At least 20 characters." << "\n";
+	}
+	else
+	{
+		cout << "The string is " << size(inputStr) << " characters." << "\n";
+		cout << "Substring: " << inputStr.substr(6, 6) << "\n";
+		inputStr.replace(10, 2, "****");
+		cout << "Replacing: " << inputStr << "\n";
+		int pos = inputStr.find("****");
+		cout << "Found **** at: " << pos << "\n";
+		inputStr = inputStr.substr(0, pos);
+		cout << "Erasing from * onward: " << inputStr << "\n";
+	}
+}
+
+void Eg3()
+{
+	string prg;
+	int word_count = 0;
+	int space_pos = -1;
+	cout << "Enter a sentence terminated by enter key:" << endl;
+	getline(cin, prg);	// getline is used to get spaces in the input
+	do {
+		space_pos = prg.find(" ", space_pos + 1);
+		if (space_pos > 0)
+			word_count++;
+	} while (space_pos > 0);
+	word_count = word_count + 1;
+	cout << "The sentence contains " << word_count << " words." << endl;
+	system("Pause");
+
+}
 
 void Task1()
 {
@@ -26,11 +67,31 @@ void Task1()
 	}
 	if (containsUsr)
 	{
-		
+		cout << "Enter a password: " << "\n";
+		char password[size];
+		cin.getline(password, size);
+		cout << "\n";
+
+		char code[9] = { 'P', 'a', '5', '5', 'w', '0', 'r', 'd', '\0' };
+
+		if (strcmp(password, code) == 0)
+		{
+			const int userLength = 24;
+			const int passwordLength = 8;
+			char identifier[userLength + passwordLength + 1] = "\0";
+			strcat_s(identifier, username);
+			strcat_s(identifier, "-\0");
+			strcat_s(identifier, password);
+			cout << identifier;
+		}
+		else
+		{
+			cout << "Sorry, the password is incorrect" << "\n";
+		}
 	}
 	else
 	{
-		cout << "Sorry, the username must contain 'usr'";
+		cout << "Sorry, the username must contain 'usr'" << "\n";
 	}
 }
 
@@ -68,6 +129,6 @@ void Eg1()
 
 int main()
 {
-	Task1();
+	Task2();
 }
 
